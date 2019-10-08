2e958d9f3a1dfb73886848020881e00d614acc11
zhaoxuyang <zhaoxuyang@songguo7.com>
2019-10-08 18:34:26 +0800

    apple-service

---
+++ apple/service/src/test/java/com/duqiaoli/apple/AppleApplicationTests.java
@@ -0,0 +1,17 @@
+package com.duqiaoli.apple;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
