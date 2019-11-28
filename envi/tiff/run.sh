1627cdbc42d210d19d52c384ca621bf642452def
zhaoxuyang <zhaoxuyang@songguo7.com>
2019-11-28 11:14:32 +0800

    nice

--- envi/tiff/run.sh
+++
@@ -1,1 +0,0 @@
-python3 /Code/project/tiff/tiff.py -i /Code/project/tiff/test.tif -x 1 -y 1
--- pippin.erc.monash.edu/web/WEB-INF/python/scripts/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-#_*_coding:utf-8_*_
-
-__all__ = ['generatePSSMProfile.py', 'generateSecondaryStructure', 'generateDisorder']
-
